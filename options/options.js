a5ba3b3c4eec1ea56a3023914130fe4dfee0428e
Crowdbotics <team@crowdbotics.com>
2023-05-15 09:19:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "weathered-violet-40691",
+  url: "https://weathered-violet-40691.botics.co",
+  api: "https://weathered-violet-40691.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
